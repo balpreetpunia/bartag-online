1663e1d644738b0585ee85550b5e6fd379331034
Balpreet Punia (surfy) <mebalpreetpunia@gmail.com>
2019-08-08 15:49:20 -0400

    Update search.php

--- search.php
+++ search.php
@@ -4,7 +4,7 @@
 if (isset($_GET['term'])){
     $return_arr = array();
     try {
-        $dbh = new PDO( "mysql:host=localhost;dbname=bartag", "root", "" );
+        $dbh = new PDO( "mysql:host=den1.mysql6.gear.host;dbname=bartag", "bartag", "Sv4Pl-18ZU!R" );
         $dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
         $stmt = $dbh->prepare('SELECT Model FROM data WHERE Model LIKE :term');
